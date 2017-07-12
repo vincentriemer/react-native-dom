e554b6391927ace4c3caa76b049bffce07ff106c
vincentriemer <vincentriemer@gmail.com>
2017-07-12 18:08:16 -0400

    move worker source declaration from inline string to preval statement

--- ReactWeb/bridge/RCTBridge.js
+++ ReactWeb/bridge/RCTBridge.js
@@ -38,53 +38,15 @@ const PARAMS = 2;
 const DEVTOOLS_FLAG = /\bdevtools\b/;
 const HOTRELOAD_FLAG = /\bhotreload\b/;
 
-let WORKER_SRC = `
-ErrorUtils = {
-  setGlobalHandler: () => {},
-  reportFatalError: console.error,
-};
+// $FlowFixMe
+let WORKER_SRC = preval`
+  const fs = require('fs');
+  const path = require('path');
 
-function sendMessage(topic, payload) {
-  postMessage(JSON.stringify({ topic, payload }));
-}
-
-var Status = undefined;
-
-onmessage = ({ data }) => {
-  const { topic, payload } = JSON.parse(data);
-  //console.log("Recieved message from main thread:", topic, payload);
-
-  switch (topic) {
-    case "loadBridgeConfig": {
-      const { config, bundle } = payload;
-
-      __fbBatchedBridgeConfig = config;
-      importScripts(bundle);
-
-      sendMessage("bundleFinishedLoading");
-      break;
-    }
-    case "callFunctionReturnFlushedQueue": {
-      const flushedQueue = __fbBatchedBridge.callFunctionReturnFlushedQueue(
-        ...payload
-      );
-      sendMessage("flushedQueue", flushedQueue);
-      break;
-    }
-    case "invokeCallbackAndReturnFlushedQueue": {
-      const flushedQueue = __fbBatchedBridge.invokeCallbackAndReturnFlushedQueue(
-        ...payload
-      );
-      sendMessage("flushedQueue", flushedQueue);
-      break;
-    }
-    case "flush":{
-      const flushedQueue = __fbBatchedBridge.flushedQueue.apply(null);
-      sendMessage("flushedQueue", flushedQueue);
-      break;
-    }
-  }
-};
+  module.exports = fs.readFileSync(
+    path.resolve(__dirname, 'RCTBridge.worker.js'),
+    'utf8'
+  );
 `;
 
 if (__DEV__) {
---
+++ ReactWeb/bridge/RCTBridge.worker.js
@@ -0,0 +1,46 @@
+ErrorUtils = {
+  setGlobalHandler: () => {},
+  reportFatalError: console.error,
+};
+
+function sendMessage(topic, payload) {
+  postMessage(JSON.stringify({ topic, payload }));
+}
+
+var Status = undefined;
+
+onmessage = ({ data }) => {
+  const { topic, payload } = JSON.parse(data);
+  //console.log("Recieved message from main thread:", topic, payload);
+
+  switch (topic) {
+    case "loadBridgeConfig": {
+      const { config, bundle } = payload;
+
+      __fbBatchedBridgeConfig = config;
+      importScripts(bundle);
+
+      sendMessage("bundleFinishedLoading");
+      break;
+    }
+    case "callFunctionReturnFlushedQueue": {
+      const flushedQueue = __fbBatchedBridge.callFunctionReturnFlushedQueue(
+        ...payload
+      );
+      sendMessage("flushedQueue", flushedQueue);
+      break;
+    }
+    case "invokeCallbackAndReturnFlushedQueue": {
+      const flushedQueue = __fbBatchedBridge.invokeCallbackAndReturnFlushedQueue(
+        ...payload
+      );
+      sendMessage("flushedQueue", flushedQueue);
+      break;
+    }
+    case "flush":{
+      const flushedQueue = __fbBatchedBridge.flushedQueue.apply(null);
+      sendMessage("flushedQueue", flushedQueue);
+      break;
+    }
+  }
+};
