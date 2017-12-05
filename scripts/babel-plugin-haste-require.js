618b74f8599b52a5aaa92bf29c04e90ff5b873e9
Vincent Riemer <vincentriemer@gmail.com>
2017-12-04 21:57:44 -0500

    reorginization and better library compiling

---
+++ scripts/babel-plugin-haste-require.js
@@ -0,0 +1,143 @@
+const path = require("path");
+const fs = require("fs");
+
+const opts = {
+  include: [path.resolve(__dirname, "..", "ReactDom")],
+  extensions: [".dom.js", ".js"]
+};
+
+function buildModuleMap(opts) {
+  const hasteMap = {};
+  const walk = function(file) {
+    const stat = fs.statSync(file);
+    if (stat.isDirectory()) {
+      fs.readdirSync(file).forEach(function(file0) {
+        if (
+          file0.indexOf([
+            // blacklist
+            "node_modules",
+            "__tests__",
+            "__mocks__",
+            "__fixtures__"
+          ]) >= 0
+        ) {
+          return;
+        }
+        walk(file + "/" + file0);
+      });
+      return;
+    }
+    if (stat.isFile()) {
+      let fileName = (/^(.*)\.js$/.exec(file) || [])[1];
+      if (!fileName) {
+        return;
+      }
+      const m = /^(.*)\.(\w+)$/.exec(fileName);
+      if (m && ["ios", "android", "native", "web"].indexOf(m[2]) >= 0) {
+        fileName = m[1];
+      }
+      const content = fs.readFileSync(file, "utf-8");
+      const moduleName = (/\* @providesModule ([\w.-]+)/.exec(content) ||
+        [])[1];
+      if (!moduleName) {
+        return;
+      }
+      if (hasteMap[moduleName] && hasteMap[moduleName] !== fileName) {
+        throw new Error(
+          "Duplicate haste module: " +
+            moduleName +
+            " in files: " +
+            fileName +
+            " & " +
+            hasteMap[moduleName]
+        );
+      }
+      hasteMap[moduleName] = fileName;
+    }
+  };
+
+  opts.include.forEach(root => {
+    const dir = root.replace(/\/\*\*$/, "");
+    walk(dir);
+  });
+
+  return hasteMap;
+}
+
+const moduleMap = buildModuleMap(opts);
+
+function mapModule(state, module) {
+  if (moduleMap.hasOwnProperty(module)) {
+    const currentDir = path.dirname(
+      path.resolve(__dirname, "..", state.file.opts.filename)
+    );
+    const relativePath = path.relative(currentDir, moduleMap[module]);
+    return "./" + relativePath;
+  }
+  return null;
+}
+
+module.exports = function({ types }) {
+  /**
+   * Will transform `require('moduleName')`.
+   */
+  function transformRequireCall(path, state) {
+    var calleePath = path.get("callee");
+    if (
+      !types.isIdentifier(calleePath.node, { name: "require" }) &&
+      !(
+        types.isMemberExpression(calleePath.node) &&
+        types.isIdentifier(calleePath.node.object, { name: "require" }) &&
+        types.isIdentifier(calleePath.node.property, { name: "requireActual" })
+      )
+    ) {
+      return;
+    }
+
+    var args = path.get("arguments");
+    if (!args.length) {
+      return;
+    }
+
+    const moduleArg = args[0];
+    if (moduleArg.node.type === "StringLiteral") {
+      const module = mapModule(state, moduleArg.node.value);
+      if (module) {
+        moduleArg.replaceWith(types.stringLiteral(module));
+      }
+    }
+  }
+
+  /**
+   * Will transform `import [kind] [identifier_name] [from] 'moduleName'`.
+   */
+  function transformImport(path, state) {
+    const source = path.get("source");
+    if (source.type === "StringLiteral") {
+      const module = mapModule(state, source.node.value);
+      if (module) {
+        source.replaceWith(types.stringLiteral(module));
+      }
+    }
+  }
+
+  return {
+    visitor: {
+      CallExpression: {
+        exit(path, state) {
+          if (path.node.seen) {
+            return;
+          }
+
+          transformRequireCall(path, state);
+          path.node.seen = true;
+        }
+      },
+      ImportDeclaration: {
+        exit(path, state) {
+          transformImport(path, state);
+        }
+      }
+    }
+  };
+};
