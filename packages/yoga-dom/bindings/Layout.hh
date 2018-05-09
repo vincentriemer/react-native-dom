45ea3099c478c6da82de3751e6759945905f8879
Vincent Riemer <vincentriemer@gmail.com>
2018-05-09 18:37:41 -0400

    move yoga-dom out of monorepo

--- packages/yoga-dom/bindings/Layout.hh
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-struct Layout {
-  double left;
-  double top;
-  
-  double width;
-  double height;
-};
--- packages/yoga-dom/rollup.config.js
+++
@@ -1,8 +0,0 @@
-import commonjs from "rollup-plugin-commonjs";
-import builtins from "rollup-plugin-node-builtins";
-
-export default {
-  input: "src/index.js",
-  context: "window",
-  plugins: [commonjs(), builtins()]
-};
