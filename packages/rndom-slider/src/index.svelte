fc16a60a73359b5b5550bb10fb5091cc5093dbaa
Moti Zilberman <motiz88@gmail.com>
2018-05-28 09:31:12 +0100

    rndom-slider: add width and height defaults

--- packages/rndom-slider/src/index.svelte
+++ packages/rndom-slider/src/index.svelte
@@ -18,42 +18,44 @@
 />
 
 <script>
-	export default {
-	  tag: "rndom-slider",
-	  data: () => ({
-	    disabled: false,
-	    value: 0,
-	    maximumValue: 1,
-	    minimumValue: 0,
-	    step: 0
-	  }),
-	  methods: {
-	    input() {
-	      const event = new CustomEvent("valueChange", {
-	        detail: { value: this.value }
-	      });
+		export default {
+		  tag: "rndom-slider",
+		  data: () => ({
+		    disabled: false,
+		    value: 0,
+		    maximumValue: 1,
+		    minimumValue: 0,
+		    step: 0,
+		    width: 0,
+		    height: 0
+		  }),
+		  methods: {
+		    input() {
+		      const event = new CustomEvent("valueChange", {
+		        detail: { value: this.value }
+		      });
 
-	      if (this.dispatchEvent(event)) {
-	        this.set({ value: this.value });
-	      }
-	    },
-	    change() {
-	      this.emitSlidingComplete();
-	    },
-	    touchEnd(e) {
-	      e.preventDefault();
-	      this.emitSlidingComplete();
-	    },
-	    mouseUp() {
-	      this.emitSlidingComplete();
-	    },
-	    emitSlidingComplete() {
-	      const event = new CustomEvent("slidingComplete", {
-	        detail: { value: this.value }
-	      });
+		      if (this.dispatchEvent(event)) {
+		        this.set({ value: this.value });
+		      }
+		    },
+		    change() {
+		      this.emitSlidingComplete();
+		    },
+		    touchEnd(e) {
+		      e.preventDefault();
+		      this.emitSlidingComplete();
+		    },
+		    mouseUp() {
+		      this.emitSlidingComplete();
+		    },
+		    emitSlidingComplete() {
+		      const event = new CustomEvent("slidingComplete", {
+		        detail: { value: this.value }
+		      });
 
-	      this.dispatchEvent(event);
-	    }
-	  }
-	};
+		      this.dispatchEvent(event);
+		    }
+		  }
+		};
 </script>
