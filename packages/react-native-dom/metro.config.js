9fb476eeebc676a405120b36eb605e8271a4ed04
Vincent Riemer <vincentriemer@gmail.com>
2018-09-08 23:33:46 -0400

    Clean up metro config

--- packages/react-native-dom/metro.config.js
+++ packages/react-native-dom/metro.config.js
@@ -1,20 +1,16 @@
 "use strict";
 
 var path = require("path");
-const blacklist = require("metro-config/src/defaults/blacklist");
 
 var config = {
   projectRoot: path.resolve(__dirname, "../.."),
-  resetCache: true,
   resolver: {
     hasteImplModulePath: require.resolve("./jest/hasteImpl"),
     platforms: ["dom"],
     providesModuleNodeModules: ["react-native-dom"],
     extraNodeModules: {
       "react-native-dom": __dirname,
-      "react-native": path.resolve(__dirname, "./node_modules/react-native"),
-      "rndom-redbox": path.resolve(__dirname, "../rndom-redbox"),
-      "rndom-switch": path.resolve(__dirname, "../rndom-switch")
+      "react-native": path.resolve(__dirname, "./node_modules/react-native")
     }
   }
 };
